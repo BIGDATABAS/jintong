b33ca385e2012c3b8d87c0a0f650764d5ff017fa
Excusep <2693224373@qq.com>
2018-09-29 15:49:09 +0800

    lyk

--- js/lyk_bagT.js
+++ js/lyk_bagT.js
@@ -2,5 +2,9 @@ $(function () {
     $(".sel .selIn li").click(function () {
         $(this).addClass("hot");
         $(this).siblings().removeClass("hot");
+
+        let date=$(this).children().eq(1).children().eq(1).text();
+        let dates=parseFloat(date).toFixed(1);
+        $("input").val(dates);
     })
 })
